fbc03cfe0c3b36f9191ac020d85b836e664a897d
CCXXXI <ccxxxi@qq.com>
2021-10-31 21:52:35 +0800

    feat: init logging

--- jinhe/app.py
+++ jinhe/app.py
@@ -1,3 +1,5 @@
+import logging
+
 import sentry_sdk
 from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
@@ -6,6 +8,11 @@ sentry_sdk.init(
     dsn="https://a7ad189559a44dd8819c0862367ecbfe@o996799.ingest.sentry.io/6042882",
     integrations=[FlaskIntegration()],
     traces_sample_rate=1.0,
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 app = Flask(__name__)
