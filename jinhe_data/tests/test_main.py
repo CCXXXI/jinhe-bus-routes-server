a9467f450939d592420b382e504125ad41344222
CCXXXI <ccxxxi@qq.com>
2021-11-19 20:46:08 +0800

    refactor: rename

--- jinhe_data/tests/test_main.py
+++ jinhe_data/tests/test_main.py
@@ -1,6 +1,6 @@
-from src.py2db import placeholder
+from src.main import placeholder
 
 
-def test_py2db():
+def test_placeholder():
     """Yet another fake test."""
     assert placeholder() is not None
