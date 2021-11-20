04fad4a5ed82e24adf82ba9bb9d8607729671cb9
CCXXXI <ccxxxi@qq.com>
2021-11-20 12:21:59 +0800

    test(data): id is unique

--- jinhe_data/tests/test_data.py
+++ jinhe_data/tests/test_data.py
@@ -35,3 +35,6 @@ def test_stations():
     """Check stations data."""
     # id > 0
     assert all(station.id > 0 for station in stations)
+
+    # id is unique
+    assert len({station.id for station in stations}) == len(stations)
