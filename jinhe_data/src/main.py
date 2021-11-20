6a78a75bcd730893f562cd0f6ba4b133bfb59117
CCXXXI <ccxxxi@qq.com>
2021-11-20 18:34:43 +0800

    feat(data): save

--- jinhe_data/src/main.py
+++ jinhe_data/src/main.py
@@ -1,3 +1,23 @@
-def placeholder():
-    """Yet another fake function."""
-    return ...
+from redis import Redis
+from redisgraph import Graph
+from tqdm import tqdm
+
+from data import routes, stations
+
+
+def save():
+    """Save all data to the database."""
+    r = Redis()
+    g = Graph("g", r)
+
+    for station in tqdm(stations, "stations"):
+        station.save(g)
+    for route in tqdm(routes, "routes"):
+        route.save(r, g)
+
+    g.commit()
+    r.save()
+
+
+if __name__ == "__main__":
+    save()
