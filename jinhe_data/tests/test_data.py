490bb87ba32684be52ae7711a52a4f65e966882e
CCXXXI <ccxxxi@qq.com>
2021-11-20 12:17:23 +0800

    test(data): remove useless test

--- jinhe_data/tests/test_data.py
+++ jinhe_data/tests/test_data.py
@@ -3,9 +3,6 @@ from data import routes, stations
 
 def test_routes():
     """Check routes data."""
-    # `directional` is True of False
-    assert all(route.directional in (0, 1) for route in routes)
-
     # `name` is unique
     assert len({route.name for route in routes}) == len(routes)
 
