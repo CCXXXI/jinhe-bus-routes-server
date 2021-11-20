33e72c2d24738e3a1a605ac1303b2bd79b88fbd2
CCXXXI <ccxxxi@qq.com>
2021-11-21 01:28:01 +0800

    test(data): rename db keys

--- jinhe_data/tests/test_main.py
+++ jinhe_data/tests/test_main.py
@@ -4,10 +4,10 @@ from src.main import g, r, save
 
 def test_save():
     """Save all data to the database."""
-    assert not r.hgetall("Route:1")
+    assert not r.hgetall("route:1")
     assert g.query("MATCH (p) RETURN count(p)").result_set[0][0] == 0
 
     save()
 
-    assert r.hgetall("Route:1")
+    assert r.hgetall("route:1")
     assert g.query("MATCH (p) RETURN count(p)").result_set[0][0] == len(stations)
