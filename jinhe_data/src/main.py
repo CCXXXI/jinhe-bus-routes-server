4435d248b2ef2b69c4218d0b1f2d747fd820a1d3
CCXXXI <ccxxxi@qq.com>
2021-11-24 12:43:18 +0800

    feat(data): save version to db

--- jinhe_data/src/main.py
+++ jinhe_data/src/main.py
@@ -1,3 +1,6 @@
+import logging
+from subprocess import run
+
 from redis import Redis
 from redisgraph import Graph
 from tqdm import tqdm
@@ -10,11 +13,19 @@ g = Graph("g", r)
 
 def save():
     """Save all data to the database."""
+    # meta
+    version = run(["git", "rev-parse", "HEAD"], capture_output=True).stdout.strip()
+    logging.info(f"{version=}")
+    r.set("version", version)
+
+    # normal
     for station in tqdm(stations, "stations"):
         station.save(g)
     g.commit()
     for route in tqdm(routes, "routes"):
         route.save(r, g)
+
+    # persistence
     r.save()
 
 
