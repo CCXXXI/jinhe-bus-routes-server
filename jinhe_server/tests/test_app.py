354754e9b226fe80670aaa2c8bafa4932d1161c5
CCXXXI <ccxxxi@qq.com>
2021-11-29 15:39:15 +0800

    refactor(server): avoid wildcard import
    In a wildcard import, all public names defined in the module are
    imported. It is advised to explicitly import the values needed or modify
    the module being imported from and define all to restrict the names to
    be imported.

--- jinhe_server/tests/test_app.py
+++ jinhe_server/tests/test_app.py
@@ -1,14 +1,14 @@
-from src.app import *
+from src import app
 
 
 def test_version():
     """The Git commit ID is a 40 digits long SHA-1 hash."""
-    assert len(version()) == 40
+    assert len(app.version()) == 40
 
 
 def test_route():
     """UC-1: 查询某条线路的基本信息"""
-    assert route("30") == {
+    assert app.route("30") == {
         "direction": "燎原-北路湾公交站",
         "oneway": "约49分",
         "directional": "1",
